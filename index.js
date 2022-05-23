1848751faf5c404e8e18d839b1bfec09101594fb
Tan Ha <79884390+tandha@users.noreply.github.com>
2022-05-23 03:41:31 -0700

    updated loader.io file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/loaderio-d01ac8bde8314c2fc7b5b47f9f8899e2.txt', (req, res) => {
-	res.sendFile('/home/ubuntu/product-overview-service/loaderio-d01ac8bde8314c2fc7b5b47f9f8899e2.txt');
+app.get('/loaderio-90f0d6bc1512bc90d526124701d2f3dc.txt', (req, res) => {
+	res.sendFile('/home/ubuntu/loaderio-authenticator/loaderio-90f0d6bc1512bc90d526124701d2f3dc.txt');
 	// res.sendFile('/Users/tanha/RPP33/SDC/product-overview-service/loaderio-07a1310218300c2fc487392e6596ba3b.txt');
 });

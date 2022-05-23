02b7b3429f33cd2ab1a3b0c9889c5b4b278e2d64
Tan Ha <79884390+tandha@users.noreply.github.com>
2022-05-23 03:53:15 -0700

    fixed the server and port

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
 app.get('/loaderio-90f0d6bc1512bc90d526124701d2f3dc.txt', (req, res) => {
 	res.sendFile('/home/ubuntu/loaderio-authenticator/loaderio-90f0d6bc1512bc90d526124701d2f3dc.txt');
 	// res.sendFile('/Users/tanha/RPP33/SDC/product-overview-service/loaderio-07a1310218300c2fc487392e6596ba3b.txt');
 });
+
+app.listen(port, () => {
+  console.log(`loaderio-authenticator app listening on port ${port}`)
+})
